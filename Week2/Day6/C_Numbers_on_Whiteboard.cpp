026086de95ff15d4f26cd7be63558bd8abf53c72
mahmudabinteasad <162348636+mahmudabinteasad@users.noreply.github.com>
2024-11-08 22:49:51 +0600

    Update C_Numbers_on_Whiteboard.cpp
    Day6

--- Week2/Day6/C_Numbers_on_Whiteboard.cpp
+++ Week2/Day6/C_Numbers_on_Whiteboard.cpp
@@ -1,28 +1,3 @@
-// #include<bits/stdc++.h>
-// using namespace std;
-// #define ll long long int
-// int main() {
-//    ios::sync_with_stdio(false);
-//    cin.tie(nullptr);
-//    int t;
-//    cin >> t;
-//    while (t--) {
-//     ll n;
-//     cin >> n;
-//     cout << "2" << '\n';
-//     cout << n << " " << n - 1 << '\n';
-//     ll x = n;
-//     ll y = n - 2;
-//     while (x >= 3 && y >= 1) {
-//         cout << x << " " << y << '\n';
-//         x--;
-//         y--;
-//     }
-//    }
-//     return 0;
-// }
-
-
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long int

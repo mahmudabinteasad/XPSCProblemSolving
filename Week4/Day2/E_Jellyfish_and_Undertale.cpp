415cb4ef577bf3ec20c4f116556284041411f126
mahmudabinteasad <162348636+mahmudabinteasad@users.noreply.github.com>
2024-11-18 21:33:52 +0600

    Update E_Jellyfish_and_Undertale.cpp

--- Week4/Day2/E_Jellyfish_and_Undertale.cpp
+++ Week4/Day2/E_Jellyfish_and_Undertale.cpp
@@ -1,8 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long int
 int main() {
    ios::sync_with_stdio(false);
    cin.tie(nullptr);
-   
+   int t;
+   cin >> t;
+   while (t--) {
+    ll a, b, n, sum = 0, mn = LLONG_MAX;
+    cin >> a >> b >> n;
+    vector<ll> v(n);
+    for (ll i = 0; i < n; i++) {
+        cin >> v[i];
+        sum += min(v[i] + 1, a) - 1;
+    }
+    cout << sum + b << '\n';
+   }
     return 0;
 }

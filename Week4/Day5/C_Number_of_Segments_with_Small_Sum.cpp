73d670a8a6f81db1c1728f5923d8bc27ae726df7
mahmudabinteasad <162348636+mahmudabinteasad@users.noreply.github.com>
2024-11-21 14:26:24 +0600

    Update C_Number_of_Segments_with_Small_Sum.cpp

--- Week4/Day5/C_Number_of_Segments_with_Small_Sum.cpp
+++ Week4/Day5/C_Number_of_Segments_with_Small_Sum.cpp
@@ -1,28 +1,67 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+//     ios::sync_with_stdio(false);
+//     cin.tie(nullptr);
+
+//     int n;
+//     long long s;
+//     cin >> n >> s;
+//     vector<int> a(n);
+//     for (int i = 0; i < n; i++) {
+//         cin >> a[i];
+//     }
+//     int l = 0;
+//     long long sum = 0;
+//     long long result = 0;
+//     for (int r = 0; r < n; r++) {
+//         sum += a[r];
+//         while (sum > s) {
+//             sum -= a[l];
+//             l++;
+//         }
+//         result += (r - l + 1);
+//     }
+//     cout << result << '\n';
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
 
-    int n;
-    long long s;
-    cin >> n >> s;
-    vector<int> a(n);
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
-    int l = 0;
-    long long sum = 0;
-    long long result = 0;
-    for (int r = 0; r < n; r++) {
-        sum += a[r];
-        while (sum > s) {
+   int n;
+   long long k;
+   cin >> n >> k;
+   vector<int> a(n);
+   for (int i = 0;i < n;i++) {
+      cin >> a[i];
+   }
+
+   int l = 0, r = 0;
+   long long sum = 0, ans = 0;
+   while (r < n) {
+      sum += a[r];
+      if (sum <= k) {
+         ans += (r - l + 1);
+      }
+      else {
+         while (sum > k && l <= r) {
             sum -= a[l];
             l++;
-        }
-        result += (r - l + 1);
-    }
-    cout << result << '\n';
-    return 0;
+         }
+         if (sum <= k) {
+            ans += (r - l + 1);
+         }
+      }
+      r++;
+   }
+
+   cout << ans << '\n';
+
+   return 0;
 }

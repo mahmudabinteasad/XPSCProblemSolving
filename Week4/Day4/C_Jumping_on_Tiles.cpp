b52631859f33e2cd775c7c023b011c51b22c684a
mahmudabinteasad <162348636+mahmudabinteasad@users.noreply.github.com>
2024-11-23 15:04:47 +0600

    Update C_Jumping_on_Tiles.cpp

--- Week4/Day4/C_Jumping_on_Tiles.cpp
+++ Week4/Day4/C_Jumping_on_Tiles.cpp
@@ -1,8 +1,31 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main() {
-   ios::sync_with_stdio(false);
-   cin.tie(nullptr);
-   
+    int t; 
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        int n = s.length();
+        char first = s[0], last = s[n - 1];
+        int d;
+        if (first < last) {
+           d = 1;
+        } else {
+           d = -1;
+        }  
+        int moves = 0; 
+        string result = "";  
+        for (char c = first; c != last + d; c += d) {
+            for (int i = 0; i < n; ++i) {
+                if (s[i] == c) {
+                    result += to_string(i + 1) + " ";  
+                    ++moves;
+                }
+            }
+        }
+        cout << abs(first - last) << " " << moves << "\n";
+        cout << result << "\n";
+    }
     return 0;
 }

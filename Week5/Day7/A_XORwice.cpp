d44bda85c065037cf7cd09c7299b7ec2f8f713a9
mahmudabinteasad <162348636+mahmudabinteasad@users.noreply.github.com>
2024-12-01 14:58:48 +0600

    Add files via upload
    Day7

---
+++ Week5/Day7/A_XORwice.cpp
@@ -0,0 +1,31 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main() {
+//    ios::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//     int t;
+//     cin >> t;
+//     while (t--) {
+//         int a, b;
+//         cin >> a >> b;
+//         int x = a & b;
+//         int result = (a ^ x) + (b ^ x);
+//         cout << result << endl;
+//     }
+//     return 0;
+// }
+
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        int a,b;
+        cin >> a >> b;
+        cout << (a ^ b) << '\n';
+    }
+    return 0;
+}
---
+++ Week5/Day7/Dull_Operation.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        cout << n << " " << n - 1 << '\n';
+    }
+    return 0;
+} // accepted

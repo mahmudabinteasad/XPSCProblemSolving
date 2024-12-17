c1bc9a2ab606cde53ca7c76026cc6dc54302b937
mahmudabinteasad <162348636+mahmudabinteasad@users.noreply.github.com>
2024-12-17 22:02:56 +0600

    Add files via upload
    Day1

---
+++ Week6/Day2/B_Permutation_Swap.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+    int t;                 
+    cin >> t;
+    while (t--) {
+      int n;               
+      cin>>n;
+ 
+      vector<int> a(n+1);
+ 
+      int ans=0;
+ 
+      for(int i=1;i<=n;i++){
+            cin>>a[i];
+      }
+ 
+      for(int i=1;i<=n;i++){
+            ans=__gcd(ans,abs(a[i]-i));
+      }
+ 
+      cout<<ans<<endl;
+ 
+   }
+    return 0;
+}

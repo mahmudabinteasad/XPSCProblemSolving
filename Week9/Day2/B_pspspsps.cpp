27e306b8ae52765492ee05206ed743958968662c
mahmudabinteasad <162348636+mahmudabinteasad@users.noreply.github.com>
2024-12-24 19:31:36 +0600

    Update B_pspspsps.cpp

--- Week9/Day2/B_pspspsps.cpp
+++ Week9/Day2/B_pspspsps.cpp
@@ -3,6 +3,32 @@ using namespace std;
 int main() {
    ios::sync_with_stdio(false);
    cin.tie(nullptr);
-   
+    int t; 
+    cin>>t;
+    while(t--){
+    int n; 
+    string s;
+    cin >> n  >> s;  
+    if (s[0] == 's') s[0] = '.';       
+    if (s.back() == 'p') s.back() = '.';  
+    bool found_p = false; 
+    bool found_s = false;   
+    for (const auto c : s) {
+        switch (c) {
+            case 'p':
+                found_p = true;  
+                break;
+            case 's':
+                found_s = true;  
+                break;
+        }
+    }
+    if (found_p && found_s) {
+        cout << "NO" << '\n';
+        } else {
+            cout << "YES" << '\n';
+            }
+    
+    }
     return 0;
 }

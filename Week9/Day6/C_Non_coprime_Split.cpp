20ac2700fc154b58a3dfdd0c7a78aa0853dc5ee8
mahmudabinteasad <162348636+mahmudabinteasad@users.noreply.github.com>
2024-12-28 18:37:59 +0600

    Update C_Non_coprime_Split.cpp

--- Week9/Day6/C_Non_coprime_Split.cpp
+++ Week9/Day6/C_Non_coprime_Split.cpp
@@ -1,6 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long int
+int findDivisor(int n) {
+    for (int i = 2; i * i <= n; ++i) {
+        if (n % i == 0) 
+            return i;
+    }
+    return n;
+}
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
@@ -9,19 +16,22 @@ int main() {
     while (t--) {
         ll l, r;
         cin >> l >> r;
-        ll a = 2, b = l - 2, found = 0;
-        for (ll i = b; i <= r - 2; i++) {
-            if ((a + i) <= r && i % a == 0) {
-                b = i;
-                found = 1;
-                break;
-            }
-        }
-        if (found == 1 && b != 0) {
-            cout << a << " " << b << '\n';
-        } else {
+        if (l <= 3 && r <= 3) {
             cout << "-1" << '\n';
-        }
+        } else if (r - l >= 1) {
+            // if (r % 2 == 0) {
+            //     cout << r / 2 << " " << r / 2 << '\n';
+            // } else if (r % 2 != 0) {
+            //     r--;
+            // cout << r / 2 << " " << r / 2 << '\n'; 
+            // }
+            cout << r / 2 << " " << r / 2 << '\n';
+        } else if (findDivisor(l) == l) {
+            cout << -1 << endl; 
+        } else {
+                cout << findDivisor(l) << " " << l - findDivisor(l) << endl;
+            }
     }
     return 0;
-} // haven't finished
+} // accepted
+

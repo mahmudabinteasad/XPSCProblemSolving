75af51ede5cf7c8d5f61c98b117542dc7b32b366
mahmudabinteasad <162348636+mahmudabinteasad@users.noreply.github.com>
2024-12-29 23:36:15 +0600

    Update D_Subarray_Sums_II.cpp

--- Week10/Day2/D_Subarray_Sums_II.cpp
+++ Week10/Day2/D_Subarray_Sums_II.cpp
@@ -7,34 +7,21 @@ int main() {
    int n, x;
    cin >> n >> x;
    vector<ll> p(n);
-   ll total = 0;
+//    ll total = 0;
    for (ll i = 0; i < n; i++) {
     cin >> p[i];
-    total += p[i];
+    // total += p[i];
     }
-    int  count = 0, j = 0;
-    if (total == x) {
-        count++;
-    }
+    map<ll, int> seen;
+    seen[0]++;
+    ll count = 0;
     ll sum = 0;
     for (int i = 0; i < n; i++) {
         sum += p[i];
-        if (sum >= x) {
-            while (sum > x) {
-                sum -= p[j];
-                j++;
-            }
-            if (sum == x) {
-                count++;
-            }
-        }
-        if (sum < x) {
-            if (p[j] < 0) {
-                sum -= p[j];
-                j++;
-            }
-        }
+        count += seen[sum - x];
+        seen[sum]++;
     }
-    cout << count << endl;
+
+    cout << count;
     return 0;
 }

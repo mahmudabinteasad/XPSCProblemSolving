74afc2ea88daf46a6004fbf1511997635a166f60
mahmudabinteasad <162348636+mahmudabinteasad@users.noreply.github.com>
2025-01-12 21:47:17 +0600

    Update B_Cover_in_Water.cpp

--- Week11/Day1/B_Cover_in_Water.cpp
+++ Week11/Day1/B_Cover_in_Water.cpp
@@ -3,6 +3,32 @@ using namespace std;
 int main() {
    ios::sync_with_stdio(false);
    cin.tie(nullptr);
-   
+   int t;
+   cin >> t;
+   while (t--) {
+    int n; 
+    cin >> n;
+   string s;
+   cin >> s;
+   int count = 0, three = 0;
+   bool flag = false;
+   for (int i = 0; i < s.size(); i++) {
+    if (s[i] == '.') {
+    count++;
+    three++;
+    } else {
+        three = 0;
+    }
+    if (three == 3) {
+        flag = true;
+        break;
+    }
+   }
+   if (flag) {
+    cout << "2" << '\n';
+   } else {
+    cout << count << '\n';
+   }
+   }
     return 0;
 }
